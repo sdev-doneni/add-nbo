a297b1900be9231d8a27cfd6d73c97bd935b6d0f
doneni <xxerrenn@icloud.com>
2024-04-06 00:03:14 +0900

    Check file size && Handle error

--- main.cpp
+++ main.cpp
@@ -24,6 +24,10 @@ void error(int errno)
 			printf("There's no matching file(s).\n");
 			exit(-1);
 			break;
+		case 3 :
+			printf("The file size must be greater than 4 bytes.\n");
+			exit(-1);
+			break;
 		default :
 			printf("Unknown error.\n");
 			exit(-1);
@@ -43,10 +47,15 @@ int main(int argc, char*argv[])
 	uint32_t num1 = 0, num2 = 0;
 	fread(&num1, sizeof(uint32_t), 1, file1);
 	fread(&num2, sizeof(uint32_t), 1, file2);
+	uint64_t tell_file1 = ftell(file1);
+	uint64_t tell_file2 = ftell(file2);
+	if(tell_file1 < 4 || tell_file2 < 4)
+		error(3);
+
 	num1 = ntohl(num1);
 	num2 = ntohl(num2);
 	uint32_t sum = num1 + num2;	
-	printf("%u(0x%x) + %u(0x%x) = %u(0x%x)\n", num1);
+	printf("%u(0x%x) + %u(0x%x) = %u(0x%x)\n", num1, num1, num2, num2, sum, sum);
 
 	return 0;
 }
